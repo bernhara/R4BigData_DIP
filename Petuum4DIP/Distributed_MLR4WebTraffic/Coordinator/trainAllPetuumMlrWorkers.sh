ca89e42dc1d651a17633bc4aba684500454298ab
bernhara <raphael.bernhard@orange.com>
2017-01-19 16:14:15 +0000

    now call a remote script which wraps the call of the worker

--- Petuum4DIP/Distributed_MLR4WebTraffic/Coordinator/trainAllPetuumMlrWorkers.sh
+++ Petuum4DIP/Distributed_MLR4WebTraffic/Coordinator/trainAllPetuumMlrWorkers.sh
@@ -11,6 +11,8 @@ then
 fi
 
 
+: ${WORKER_DEFAULT_INSTALL_DIR:=R4BigData_DIP/Petuum4DIP/Distributed_MLR4WebTraffic/Worker}
+
 Usage ()
 {
     if [ -n "$1" ]
@@ -18,7 +20,7 @@ Usage ()
 	echo "ERROR: $1" 1>&2
     fi
     echo "Usage: ${CMD} [--dryrun] <worker specification> [<worker specification>]*" 1>&2
-    echo "with <worker specification> having the following form: [<remote user>@]<worker hostname>[:<mlr remote folder installation dir if different>]" 1>&2
+    echo "with <worker specification> having the following form: [<remote user>@]<worker hostname>[:<mlr remote folder installation dir if differs from default>]" 1>&2
     echo "NOTES: workers are indexed in appearing order (first specified worker has index 0)" 1>&2
     exit 1
 }
@@ -43,7 +45,7 @@ while [ -n "$1" ]
 do
     worker_specification="$1"
 
-    worked_index="${list_index}"
+    worker_index="${list_index}"
 
     worker_ssh_specification="${worker_specification%:*}"
     worker_ssh_hostname="${worker_ssh_specification#*@}"
@@ -58,7 +60,7 @@ do
     if [ "${worker_ssh_remote_path_specification}" = "${worker_specification}" ]
     then
 	# no remote path specified => same path
-	worker_ssh_remote_path_specification=$( realpath "${HERE}" )
+	worker_ssh_remote_path_specification="${WORKER_DEFAULT_INSTALL_DIR}"
     fi
 
     petuum_workers_specification_list[${worker_index}]="'${list_index}' '${worker_ssh_remote_user}' '${worker_ssh_hostname}' '${worker_ssh_remote_path_specification}'"
@@ -100,6 +102,28 @@ mkdir -p "${tmp_dir}"
 : ${petuum_interworker_tcp_port:=9999}
 num_clients=${#petuum_workers_specification_list[@]}
 
+build_worker_launcher_common_args () {
+
+    worker_launcher_common_args=''
+
+    for worker_specification in "${petuum_workers_specification_list[@]}"
+    do
+        # transform the list into an array (all elements are quoted to handle empty elements (=> eval)
+	eval worker_specification_array=( ${worker_specification} )
+	worker_index="${worker_specification_array[0]}"
+	worker_ssh_remote_user="${worker_specification_array[1]}"
+	worker_ssh_hostname="${worker_specification_array[2]}"
+	worker_ssh_remote_path_specification="${worker_specification_array[3]}"
+
+	worker_launcher_common_args="${worker_launcher_common_args} ${worker_ssh_hostname}:${petuum_interworker_tcp_port}"
+    done
+
+    echo "${worker_launcher_common_args}"
+
+}
+
+worker_launcher_common_args=$( build_worker_launcher_common_args )
+
 build_worker_mlr_cmd () {
 
     worker_index="${worker_specification_array[0]}"
@@ -114,22 +138,8 @@ build_worker_mlr_cmd () {
 	worker_ssh_remote_specification="${worker_ssh_hostname}"
     fi
 
-    local_generate_learning_data_command="./generateMLRLearningData.sh ${tmp_dir}/libsvm_access_log.txt -l ${tmp_dir}/labels.txt"
-    local_worker_mlr_command="\
-GLOG_logtostderr=true GLOG_v=-1 GLOG_minloglevel=0 \
-/share/Petuum/bosen/app/mlr/bin/mlr_main \
---num_comm_channels_per_client=1 \
---staleness=2 --client_id=0 --num_app_threads=1 \
---num_clients=${num_clients} \
---use_weight_file=false --weight_file= \
---num_batches_per_epoch=10 --num_epochs=40 \
---output_file_prefix=${tmp_dir}/rez \
---lr_decay_rate=0.99 --num_train_eval=10000 \
---global_data=true \
---init_lr=0.01 \
---num_test_eval=20 --perform_test=false --num_batches_per_eval=10 --lambda=0 \
---hostfile=${tmp_dir}/localserver \
---train_file=${tmp_dir}/libsvm_access_log.txt \
+    local_worker_command="\
+./trainWorker.sh ${worker_index} ${worker_launcher_common_args} \
 "
 
     remote_command="ssh \
@@ -139,31 +149,13 @@ ${worker_ssh_remote_specification} \
 '/bin/bash -c \"\
 cd ${worker_ssh_remote_path_specification} && \
 \
-${local_generate_learning_data_command} && \
-\
-${local_worker_mlr_command}\
+${local_worker_command}\
 \"\
 '\
 "
 
     echo "${remote_command}"
 }
-
-# generate server file
-(
-    for worker_specification in "${petuum_workers_specification_list[@]}"
-    do
-	
-	# transform the list into an array (all elements are quoted to handle empty elements (=> eval)
-	eval worker_specification_array=( ${worker_specification} )
-	worker_index="${worker_specification_array[0]}"
-	worker_ssh_remote_user="${worker_specification_array[1]}"
-	worker_ssh_hostname="${worker_specification_array[2]}"
-	worker_ssh_remote_path_specification="${worker_specification_array[3]}"
-
-	echo ${worker_index} ${worker_ssh_hostname} ${petuum_interworker_tcp_port}
-    done
-) > ${tmp_dir}/localserver
 
 # lauch all workers
 
