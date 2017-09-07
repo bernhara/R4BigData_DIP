d54fe399df3ff647a502a4bd09b8aa7bb9cb3cfe
bernhara <raphael.bernhard@orange.com>
2017-09-07 16:39:18 +0200

    do not use anymore symbolic links to call wrapper

--- Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/etc/_python_wrapper.sh
+++ Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/etc/_python_wrapper.sh
@@ -3,11 +3,15 @@
 : ${PYTHON:="python3"}
 
 here=`dirname "$0"`
+wrapped_command="$1"
+shift 1
 
-this_command=`basename "$0" ".sh"`
+
+corresponding_python_main=`basename "${wrapped_command}" ".sh"`
 
 : ${python_src_dir:="${here}/../src"}
 
-: ${python_main:="${python_src_dir}/${this_command}.py"}
+: ${python_main:="${python_src_dir}/${corresponding_python_main}.py"}
 
 ${PYTHON} "${python_main}" "$@"
+
