9dcbca0c6a096c1d9977449e9bd26feb6419f800
bernhara <raphael.bernhard@orange.com>
2018-04-19 13:20:20 +0200

    first run OK

--- Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/setupProjectWithPipenv.py
+++ Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/setupProjectWithPipenv.py
@@ -11,19 +11,21 @@ os.putenv("https_proxy", "http://proxy:8080")
 quoted_sys_executable = '"%s"' % sys.executable
 
 #!! zz = 'TT'.join('a', 'b')
-path_prefix_elements = [sys.base_exec_prefix, os.path.join(sys.base_exec_prefix, 'Scripts')]
+path_prefix_element_list = [sys.base_exec_prefix, os.path.join(sys.base_exec_prefix, 'Scripts')]
 
-quoted_path_prefix_elements = ("\"%s\"" % p for p in path_prefix_elements)
+quoted_path_prefix_list = ["\"%s\"" % p for p in path_prefix_element_list]
 
-path_prefix = os.pathsep.join(quoted_path_prefix_elements)
+path_prefix = os.pathsep.join(quoted_path_prefix_list)
+path_prefix = os.pathsep.join(path_prefix_element_list)
 
 PATH = os.getenv('PATH')
 new_PATH = os.pathsep.join ([path_prefix, PATH])
 os.putenv("PATH", new_PATH)
 
-pipenv_launch_command = quoted_sys.executable + ' -m pipenv ' + '--python ' + quoted_sys.executable
+pipenv_launch_command = quoted_sys_executable + ' -m pipenv ' + '--python ' + quoted_sys_executable
 
-command =  pipenv_launch_command + ' install --dev numpy'
+command =  '"' + pipenv_launch_command + ' install --dev numpy"' 
+#!!command = '"set"'
 
 if __name__ == '__main__':
     status = os.system(command)
