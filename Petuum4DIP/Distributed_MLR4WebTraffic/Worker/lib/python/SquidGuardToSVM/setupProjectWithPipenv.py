63e0b1e7f56595f0125326bc0e6504ecedfbd717
bernhara <raphael.bernhard@orange.com>
2018-04-19 14:36:02 +0200

    refactoring

--- Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/setupProjectWithPipenv.py
+++ Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/setupProjectWithPipenv.py
@@ -1,7 +1,5 @@
-import re
 import sys
 import os
-
 import subprocess
 
 os.putenv("PIPENV_VENV_IN_PROJECT", "1")
@@ -12,25 +10,21 @@ os.putenv("https_proxy", "http://proxy:8080")
 
 quoted_sys_executable = '"%s"' % sys.executable
 
-#!! zz = 'TT'.join('a', 'b')
+# compute additional PATH elements
 path_prefix_element_list = [sys.base_exec_prefix, os.path.join(sys.base_exec_prefix, 'Scripts')]
-
-quoted_path_prefix_list = ["\"%s\"" % p for p in path_prefix_element_list]
-
-path_prefix = os.pathsep.join(quoted_path_prefix_list)
 path_prefix = os.pathsep.join(path_prefix_element_list)
 
+# prepend PATH system var with new elements, so that current Python has priority while searching in the PATH
 PATH = os.getenv('PATH')
 new_PATH = os.pathsep.join ([path_prefix, PATH])
 os.putenv("PATH", new_PATH)
 
+# prepare the command to launch
 pipenv_launch_command = quoted_sys_executable + ' -m pipenv ' + '--python ' + quoted_sys_executable
 
-command =  '"' + pipenv_launch_command + ' install --dev numpy"'
-command =  pipenv_launch_command + ' install --dev numpy' 
-#!!command = '"set"'
+command_to_call =  pipenv_launch_command + ' install --dev' 
 
 if __name__ == '__main__':
-    status = subprocess.call(command)
+    status = subprocess.call(command_to_call)
     sys.exit (status)
     
