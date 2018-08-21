69a695b55873a0142fa2737a28954add85172487
BERNHARD Raphael <raphael.bernhard@orange.com>
2018-08-21 11:43:53 +0200

    add methods not detected previously

--- SquidGuardClassifier/translateAccessLogToSquidGuardInput.sh
+++ SquidGuardClassifier/translateAccessLogToSquidGuardInput.sh
@@ -1,7 +1,14 @@
 #! /bin/bash
 
 awk '
-    $6 == "POST" {print $7" "$3"/ - - "$6}
-    $6 == "GET" {print $7" "$3"/ - - "$6}
-    $6 == "CONNECT" {print "https://"$7" "$3"/ - - GET"}
+    {matched=0}
+    $6 == "POST" {print $7" "$3"/ - - "$6; matched=1}
+    $6 == "GET" {print $7" "$3"/ - - "$6; matched=1}
+    $6 == "PROPFIND" {print $7" "$3"/ - - "$6; matched=1}
+    $6 == "HEAD" {print $7" "$3"/ - - "$6; matched=1}
+    $6 == "OPTIONS" {print $7" "$3"/ - - "$6; matched=1}
+    $6 == "REPORT" {print $7" "$3"/ - - "$6; matched=1}
+    $6 == "CONNECT" {print "https://"$7" "$3"/ - - GET"; matched=1}
+    $6 == "NONE" {print "http://_bad_request_ "$3"/ - - GET"; matched=1}
+    matched==0 {print "http://_bad_input_log_ "$3"/ - - GET"; matched=1}
 '
