188908e676eeac00558666b0714bf6df44e9bcc3
bernhara <raphael.bernhard@orange.com>
2018-09-11 10:25:31 +0200

    Add a "work around" to a squidGuad bug which mishandle %0A in URLs => replace %0A before passing it to squidGuard

--- SquidGuardClassifier/translateAccessLogToSquidGuardInput.sh
+++ SquidGuardClassifier/translateAccessLogToSquidGuardInput.sh
@@ -1,14 +1,43 @@
 #! /bin/bash
 
+# -*- mode: awk -*-
+
 awk '
-    {matched=0}
-    $6 == "POST" {print $7" "$3"/ - - "$6; matched=1}
-    $6 == "GET" {print $7" "$3"/ - - "$6; matched=1}
-    $6 == "PROPFIND" {print $7" "$3"/ - - "$6; matched=1}
-    $6 == "HEAD" {print $7" "$3"/ - - "$6; matched=1}
-    $6 == "OPTIONS" {print $7" "$3"/ - - "$6; matched=1}
-    $6 == "REPORT" {print $7" "$3"/ - - "$6; matched=1}
-    $6 == "CONNECT" {print "https://"$7" "$3"/ - - GET"; matched=1}
-    $6 == "NONE" {print "http://_bad_request_ "$3"/ - - GET"; matched=1}
-    matched==0 {print "http://_bad_input_log_ "$3"/ - - GET"; matched=1}
+
+{
+    gsub ("%0A", "_0A_")
+}
+
+{
+    matched=0
+}
+
+$6 == "POST" {
+    print $7" "$3"/ - - "$6; matched=1
+}
+$6 == "GET" {
+    print $7" "$3"/ - - "$6; matched=1
+}
+$6 == "PROPFIND" {
+    print $7" "$3"/ - - "$6; matched=1
+}
+$6 == "HEAD" {
+    print $7" "$3"/ - - "$6; matched=1
+}
+$6 == "OPTIONS" {
+    print $7" "$3"/ - - "$6; matched=1
+}
+$6 == "REPORT" {
+    print $7" "$3"/ - - "$6; matched=1
+}
+$6 == "CONNECT" {
+    print "https://"$7" "$3"/ - - GET"; matched=1
+}
+$6 == "NONE" {
+    print "http://_bad_request_ "$3"/ - - GET"; matched=1
+}
+
+matched==0 {
+    print "http://_bad_input_log_ "$3"/ - - GET"; matched=1
+}
 '
