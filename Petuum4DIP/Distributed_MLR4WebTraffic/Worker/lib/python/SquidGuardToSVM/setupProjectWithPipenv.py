486ef9b1e3f502d6748f9e93b5c14c52f6b5b7c9
bernhara <raphael.bernhard@orange.com>
2019-01-24 18:27:20 +0100

    works interactively but not in script

--- Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/setupProjectWithPipenv.py
+++ Petuum4DIP/Distributed_MLR4WebTraffic/Worker/lib/python/SquidGuardToSVM/setupProjectWithPipenv.py
@@ -13,6 +13,7 @@ quoted_sys_executable = '"%s"' % sys.executable
 
 # compute additional PATH elements
 path_prefix_element_list = [sys.base_exec_prefix, os.path.join(sys.base_exec_prefix, 'Scripts')]
+
 path_prefix = os.pathsep.join(path_prefix_element_list)
 
 # prepend PATH system var with new elements, so that current Python has priority while searching in the PATH
@@ -23,23 +24,9 @@ os.putenv("PATH", new_PATH)
 # prepare the command to launch
 pipenv_launch_command = quoted_sys_executable + ' -m pipenv ' + '--python ' + quoted_sys_executable
 
-# check ifo pipenv environment already exists
-check_venv_command = pipenv_launch_command + ' --venv'
+print ("install or upgrade current venv", flush=True)
 
-p = subprocess.run(check_venv_command, shell=False, check=False, universal_newlines=True)
-print ("DONE")
-print (p.returncode)
-print (stdout)
-
-if p.returncode == 1:
-
-    create_venv_command =  pipenv_launch_command + ' install' 
-    p = subprocess.run(create_venv_command, shell=False, check=False, universal_newlines=True)
-    
-else:
-    
-    update_venv_command =  pipenv_launch_command + ' sync' 
-    p = subprocess.run(update_venv_command, shell=False, check=False, universal_newlines=True)    
-    
-
-
+install_venv_command =  pipenv_launch_command + ' install'
+install_venv_command =  pipenv_launch_command + ' update'
+print ('running: ' + install_venv_command, flush=True) 
+p = subprocess.run(install_venv_command, shell=False, check=False, universal_newlines=True)    
